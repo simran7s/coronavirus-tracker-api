f751834b6e5be2df49eb930cc64a9b68b303c54d
Simran Sandhu <simran7s@my.yorku.ca>
2021-08-14 13:47:02 -0400

    Update dependencies (FastAPI, uvicorn, etc.) (#337) * update `generate_reqs` invoke task for latest pipenv * fastapi 0.61 -> 0.63 * uvicorn 0.11.8 -> 0.13.4 * sentry, scout updates, etc * update requirements test * update Sources enum * linting fix (invalid-name) * use Sources enum instead of string * skip test_requirements if not python3.8

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -3,12 +3,24 @@ from ..services.location.csbs import CSBSLocationService
 from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
-# Mapping of services to data-sources.
-DATA_SOURCES = {
-    "jhu": JhuLocationService(),
-    "csbs": CSBSLocationService(),
-    "nyt": NYTLocationService(),
-}
+
+class DataSourceSingleton:
+    _instance = None
+
+    def __new__(self):
+        # creates new DataSourceSingleton instance iff one does not exist.
+        # Then returns the new/existing instance
+        if not DataSourceSingleton._instance:
+            DataSourceSingleton._instance = super(
+                DataSourceSingleton, self).__new__(self)
+
+            # Mapping of services to data-sources.
+            self._DATA_SOURCES = {
+                "jhu": JhuLocationService(),
+                "csbs": CSBSLocationService(),
+                "nyt": NYTLocationService(),
+            }
+        return DataSourceSingleton._instance
 
 
 def data_source(source):
@@ -18,4 +30,6 @@ def data_source(source):
     :returns: The service.
     :rtype: LocationService
     """
-    return DATA_SOURCES.get(source.lower())
+
+    storedSources = DataSourceSingleton()
+    return storedSources._DATA_SOURCES.get(source.lower())
